9ad82a3508de9ffa58a0e46e5b60230ca696a7ab
Ennio Marke <mystical.ennio@gmail.com>
2024-08-21 12:50:51 +0200

    new port

--- web-server.js
+++ web-server.js
@@ -23,7 +23,7 @@ export default class webServer {
     }
     start() {
         this.loadRoutes();
-        this.app.listen(3000, () => {
+        this.app.listen(3241, () => {
             console.log('Server is running on http://127.0.0.1:3000');
         });
     }
