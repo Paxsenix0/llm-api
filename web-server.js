e5f661cc55972c032856d3e8dbadd7646c78fd9b
Alex <a24dzn@gmail.com>
2025-02-27 19:51:43 +0800

    Update web-server.js

--- web-server.js
+++ web-server.js
@@ -23,8 +23,8 @@ export default class webServer {
     }
     start() {
         this.loadRoutes();
-        this.app.listen(3000, () => {
-            console.log('Server is running on http://127.0.0.1:3000');
+        this.app.listen(7860, () => {
+            console.log('Server is running on http://127.0.0.1:7860');
         });
     }
 }
